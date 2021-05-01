4ac476d34a74ab479661f5d664736f86f90354eb
Daoqi Lai <604869221@qq.com>
2021-05-01 19:56:10 +0800

    Update main.v

--- main.v
+++ main.v
@@ -1,8 +1,8 @@
 fn main() {
-    introduction := 'My name is Van. \
-        I am an artist. \
-        I am a performance artist. \
-        I am hired for people to fulfill their fantasies, their deep, dark fantasies.'
+    introduction := 'My name is Van. ' +
+        'I am an artist. ' +
+        'I am a performance artist. ' +
+        'I am hired for people to fulfill their fantasies, their deep, dark fantasies.'
 
     println(introduction)
 }
